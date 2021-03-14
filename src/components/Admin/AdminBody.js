084d7e381fc15d26a1c7e0ec0477cff68f2a1ae8
philip_owusu_afriyie <philip.afriyie@ashesi.edu.gh>
2021-03-14 20:14:40 +0000

    Interview Updates

--- src/components/Admin/AdminBody.js
+++ src/components/Admin/AdminBody.js
@@ -35,28 +35,90 @@ class AdminBody extends Component {
     render(){
         return (
             <>
-            {this.state.loading ? <div><WindMillLoading /> <p id="loading">Loading . . . </p></div> :
-            <div>
-            <div className="admin-card">
-                <div className="card text-white bg-warning mb-3" style={{ maxWidth: '18rem' }}>
-                    <div className="card-header">Current Number of Events</div>
-                    <div className="card-body">
-                        <h5 className="card-title" style={{textAlign: 'center'}}>{this.state.events.length}</h5>
-                    </div>
-                </div>
-            </div>
+                {this.state.loading ? <div><WindMillLoading /> <p id="loading">Loading . . . </p></div> :
+                    <div>
+                        <a data-toggle="modal" data-target=".events-modal" href={"###"}>
+                            <div className="admin-card">
+                                <div className="card text-white bg-warning mb-3" style={{ maxWidth: '18rem' }}>
+                                    <div className="card-header">Current Number of Events</div>
+                                    <div className="card-body">
+                                        <h5 className="card-title" style={{textAlign: 'center'}}>{this.state.events.length}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
 
-            <div className="admin-card">
-            <div className="card text-white bg-warning mb-3" style={{ maxWidth: '18rem' }}>
-                <div className="card-header">Current Number of Users</div>
-                <div className="card-body">
-                    <h5 className="card-title" style={{textAlign: 'center'}}>{this.state.users.length}</h5>
-                </div>
-            </div>
-        </div>
-        </div>
-            }
-            </>
+                        <a data-toggle="modal" data-target=".users-modal" href={"###"}>
+                            <div className="admin-card">
+                                <div className="card text-white bg-warning mb-3" style={{ maxWidth: '18rem' }}>
+                                    <div className="card-header">Current Number of Users</div>
+                                    <div className="card-body">
+                                        <h5 className="card-title" style={{textAlign: 'center'}}>{this.state.users.length}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+
+                        <div className="modal fade users-modal" tabIndex="-1" role="dialog" aria-hidden="true">
+                            <div className="modal-dialog modal-lg">
+                                <div className="modal-content">
+                                    <table className="table table-hover">
+                                        <thead>
+                                        <tr>
+                                            <th scope="col">First Name</th>
+                                            <th scope="col">Last Name</th>
+                                            <th scope="col">Username</th>
+                                            <th scope="col">Email</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {
+                                            this.state.users.map((user, index) => (
+                                                <tr key={index}>
+                                                    <td><p>{user.first_name}</p></td>
+                                                    <td><p>{user.last_name}</p></td>
+                                                    <td><p>{user.username}</p></td>
+                                                    <td><p>{user.email}</p></td>
+                                                </tr>
+                                            ))
+                                        }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="modal fade events-modal" tabIndex="-1" role="dialog" aria-hidden="true">
+                            <div className="modal-dialog modal-lg">
+                                <div className="modal-content">
+                                    <table className="table table-hover">
+                                        <thead>
+                                        <tr>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Venue</th>
+                                            <th scope="col">Room Capacity</th>
+                                            <th scope="col">Current Seat Number</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {
+                                            this.state.events.map((event, index) => (
+                                                <tr key={index}>
+                                                    <td><p>{event.title}</p></td>
+                                                    <td><p>{event.venue}</p></td>
+                                                    <td><p>{event.room_capacity}</p></td>
+                                                    <td><p>{event.current_seat_number}</p></td>
+                                                </tr>
+                                            ))
+                                        }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                }
+                </>
         )
     }
 }
--- src/components/Admin/EventDetails.js
+++ src/components/Admin/EventDetails.js
@@ -65,52 +65,139 @@ class EventDetails extends Component {
                         icon: "warning",
                     });
                 }
-                // window.location.reload();
             } else {
                 swal("No action performed!");
             }
         });
     }
+
+    handleEditEvent = async (event_id) => {
+        return alert(event_id);
+        const url = `http://localhost:4000/admin//editevent/${event_id}`;
+        const token = `Bearer ${JSON.parse(localStorage.getItem('token'))}`;
+    }
+
     render() {
         return (
             <>
                 <AdminHeader></AdminHeader>
-                {this.state.loading? <div><WindMillLoading /><p id="loading">Loading...</p></div> : 
-                <div className="container mt-5">
-                <table className="table table-hover table-light">
-                <thead>
-                    <tr>
-                    <th scope="col">Title</th>
-                    <th scope="col">Venue</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Speaker</th>
-                    <th scope="col">Room Capacity</th>
-                    <th scope="col">Current Seat Number</th>
-                    <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    this.state.data.map((event, index) => (
-                    <tr key={index}>
-                    <td>{event.title}</td>
-                    <td>{event.venue}</td>
-                    <td>{moment(event.date).format('DD/MM/YYYY')}</td>
-                    <td>{event.speaker}</td>
-                    <td>{event.room_capacity}</td>
-                    <td>{event.current_seat_number}</td>
-                    <td>
-                    <Icon className='pointer' icon={eyeOutlined} />
-                    <Icon className='pointer' id='edit' icon={editIcon} />
-                    <Icon className='pointer' id='delete' icon={trashcanIcon} onClick={() => this.handleDeleteEvent(event._id)}/>
-                    </td>
-                    </tr>
-                    ))}
-                </tbody>
-                </table>
+                {this.state.loading? <div><WindMillLoading /><p id="loading">Loading...</p></div> :
+                    <div className="container mt-5">
+                        <table className="table table-hover table-light">
+                            <thead>
+                            <tr>
+                                <th scope="col">Title</th>
+                                <th scope="col">Venue</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Speaker</th>
+                                <th scope="col">Room Capacity</th>
+                                <th scope="col">Current Seat Number</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            {this.state.data.map((event, index) => (
+                                <tr key={index}>
+                                    <td>{event.title}</td>
+                                    <td>{event.venue}</td>
+                                    <td>{moment(event.date).format('DD/MM/YYYY')}</td>
+                                    <td>{event.speaker}</td>
+                                    <td>{event.room_capacity}</td>
+                                    <td>{event.current_seat_number}</td>
+                                    <td>
+                                        <a data-toggle="modal" data-target=".events-modal" href={"###"}><Icon className='pointer' icon={eyeOutlined} /></a>
+                                        <a data-toggle="modal" data-target="#edit_event"><Icon className='pointer' id='edit' icon={editIcon} /></a>
+                                        <Icon className='pointer' id='delete' icon={trashcanIcon} onClick={() => this.handleDeleteEvent(event._id)}/>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                }
+                <div className="modal fade events-modal" tabIndex="-1" role="dialog" aria-hidden="true">
+                    <div className="modal-dialog modal-lg">
+                        <div className="modal-content">
+                            <table className="table table-hover">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Venue</th>
+                                    <th scope="col">Room Capacity</th>
+                                    <th scope="col">Current Seat Number</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    this.state.data.map((event, index) => (
+                                        <tr key={index}>
+                                            <td><p>{event.title}</p></td>
+                                            <td><p>{event.venue}</p></td>
+                                            <td><p>{event.room_capacity}</p></td>
+                                            <td><p>{event.current_seat_number}</p></td>
+                                        </tr>
+                                    ))
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-            }
-            </>
+
+
+
+                <div className="modal fade" id="edit_event" tabIndex="-1" role="dialog"
+                     aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                {
+                                    this.state.data.map((event, index) => (
+                                        <div key={index}>
+                                            <h5 className="modal-title" id="exampleModalLabel">{event.title}</h5>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                            <div className="modal-body">
+                                <form>
+                                    {
+                                        this.state.data.map((event, index) => (
+                                            <div key={index}>
+                                                <div className="form-group">
+                                                    <label htmlFor="recipient-name" className="col-form-label">Title:</label>
+                                                    <input type="text" className="form-control" id="recipient-name" value={event.title} onChange={(e)=> e.target.value}/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="recipient-name" className="col-form-label">Venue:</label>
+                                                    <input type="text" className="form-control" id="recipient-name" value={event.venue} onChange={(e)=> e.target.value}/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="recipient-name" className="col-form-label">Date:</label>
+                                                    <input type="text" className="form-control" id="recipient-name" value={event.date} onChange={(e)=> e.target.value}/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="recipient-name" className="col-form-label">Speaker:</label>
+                                                    <input type="text" className="form-control" id="recipient-name" value={event.speaker} onChange={(e)=> e.target.value}/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="recipient-name" className="col-form-label">Room Capacity:</label>
+                                                    <input type="text" className="form-control" id="recipient-name" value={event.room_capacity} onChange={(e)=> e.target.value}/>
+                                                </div>
+                                            </div>
+                                        ))
+                                    }
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </>
         )
     }
 }
